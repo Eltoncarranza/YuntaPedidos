a8ba6c32437ab4d664418665a809372039057b5c
ELTONCARRANZA <i202330305@cibertec.edu.pe>
2026-04-05 04:28:53 -0500

    first commit

---
+++ Causa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Causa
+//
+//  Created by Pi6u89 on 1/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
