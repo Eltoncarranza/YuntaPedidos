b91c6aedf9fa06f67c985ba1a6df5f03a90ffb4c
ELTONCARRANZA <i202330305@cibertec.edu.pe>
2026-04-24 20:56:43 -0500

    Noticias, Perfil

---
+++ Causa/Noticia.swift
@@ -0,0 +1,8 @@
+//
+//  Noticia.swift
+//  Causa
+//
+//  Created by Pi6u89 on 24/04/26.
+//
+
+import Foundation
---
+++ Causa/Producto.swift
@@ -0,0 +1,8 @@
+//
+//  Producto.swift
+//  Causa
+//
+//  Created by Pi6u89 on 24/04/26.
+//
+
+import Foundation
