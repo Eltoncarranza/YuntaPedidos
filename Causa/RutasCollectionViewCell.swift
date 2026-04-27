283f1ab4524653ed9b0061685982fe9685523bb8
ELTONCARRANZA <i202330305@cibertec.edu.pe>
2026-04-27 17:59:50 -0500

    app completa Vercion 1.0.0

---
+++ Causa/RutasCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RutasCollectionViewCell.swift
+//  Causa
+//
+//  Created by Pi6u89 on 27/04/26.
+//
+
+import UIKit
+
+class RutasCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Causa/RutasTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RutasTableViewCell.swift
+//  Causa
+//
+//  Created by Pi6u89 on 27/04/26.
+//
+
+import UIKit
+
+class RutasTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
